b00216239da4c666348a586c5a29566b09c51877
lafreeZhaZha <lafree317@163.com>
2017-05-30 10:22:53 +0800

    createProject

---
+++ Poker/Assets/Script/PokerController.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * 
+ *      Title:翻牌游戏
+ * 
+ *      Description:Unity学习项目
+ * 
+ *      Date:2017年5月30日10:19:44
+ * 
+ *      Version:1
+ * 
+ *      Modify Recoder:
+ * 
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
